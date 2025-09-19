2c2518bd2fb390e38eff1b17358f81d1bfbc57c7
ice <ice@box.local>
2025-09-19 20:48:14 +0100

    setup stage 2 translation and done some reformatting

--- src/main.c
+++ src/main.c
@@ -3,7 +3,12 @@
 #include "vm.h"
 
 const uint32_t _guest_payload[] = {
-    0xd4000002  /* hvc #0 */
+    0x580000a1,     /* ldr x1, #20   */
+    0x52800922,     /* mov w2, #'G'  */
+    0x39000022,     /* strb w2, [x1] */
+    0xd4000002,     /* hvc #0        */
+    0x14000000,     /* b .           */
+    0x09000000,     /* literal for UART addr */
 };
 
 /* base addr of the PL011 UART in qemu's 'virt' machine */
@@ -30,100 +35,96 @@ uint64_t va_2_pa_el2(uint64_t va);
 extern void __exception_vectors(void);
 
 /*      write sysregs      */
-void __write_vbar_el2 (uint64_t val);
-void __write_tcr_el2  (uint64_t val);
-void __write_hcr_el2  (uint64_t val);
-void __write_cptr_el2 (uint64_t val);
-void __write_ttbr0_el2(uint64_t val);
-void __write_sctlr_el2(uint64_t val);
+void __write_vbar_el2  (uint64_t val);
+void __write_tcr_el2   (uint64_t val);
+void __write_hcr_el2   (uint64_t val);
+void __write_cptr_el2  (uint64_t val);
+void __write_ttbr0_el2 (uint64_t val);
+void __write_sctlr_el2 (uint64_t val);
+void __write_vttbr_el2 (uint64_t val);
+void __write_vtcr_el2  (uint64_t val);
 
 /*      read sysregs       */
 uint64_t __read_sctlr_el2(void);
 
-/* PT data                       */
-/*      L1-3D descrip table      */
+/* PT data . stage 1 (for hypv) */
 #define PTE_VALID     (1UL << 0)
 #define PTE_TABLE     (1UL << 1)
-
-/*      L1-2 descrip block      
-          `-> PTE_MEM_ATTR_IDX(): MAIR idx 
-          `-> PTE_AF: access flag
-          `-> PTE_SH_IS: inner shareable
-          `-> PTE_AP_RW_EL2: EL2 R/W      */
 #define PTE_BLOCK     (0UL << 1)
 #define PTE_MEM_ATTR_IDX(x) ((x) << 2)
 #define PTE_AF        (1UL << 10)
 #define PTE_SH_IS     (3UL << 8)
 #define PTE_AP_RW_EL2 (0UL << 6)
 
-/* mem attribs fo MAIR_EL2 
-    `-> ATTR0: device-nGnRE mem
-    `-> ATTR1: normal, in/outer WB/WA/RA */
+/* mem attribs for MAIR_EL2 
+    `--> ATTR0: device-nGnRE mem
+    `--> ATTR1: normal, in/outer WB/WA/RA */
 #define MAIR_ATTR0_DEV    (0x04)
 #define MAIR_ATTR1_NORM   (0xff)
 
 /* TCR_EL2 registers */
-#define TCR_EL2_T0SZ(x)   ((x) & 0x3F)      /* TnSZ = 64 - x */
+#define TCR_EL2_T0SZ(x)  ((x) & 0x3F)   /* TnSZ = 64 - x */
 #define TCR_EL2_PS_40_BIT (2UL << 16)
 #define TCR_EL2_TG0_4K    (0UL << 14)
 #define TCR_EL2_SH0_IS    (3UL << 12)
 #define TCR_EL2_ORGN0_WB  (1UL << 10)
-#define TCR_EL2_IRGN0_WB  (1UL << 8)
+#define TCR_EL2_IRGN0_WB   (1UL << 8)
 
-/* HCR_EL2 bits 
-    `-> HCR_EL2_RW_BIT: set EL1 to be arm4 */
+/* HCR_EL2 bit to enable stage 2 translation 
+   for guest*/
 #define HCR_EL2_RW_BIT  (1UL << 31)
-#define HCR_EL2_TGE_BIT (1UL << 27)
+#define HCR_EL2_VM_BIT  (1UL << 0)
+// #define HCR_EL2_TGE_BIT (1UL << 27)
 // #define HCR_EL2_TWI_BIT (1UL << 20)
 
-/* page tables
-    create identity map for the first 2GB of mem
-    L0 -> L1 -> 1GB block 
-    L0 tbl (1entr) -> L1 tbl
-    L1 tbl (2entr) -> 2 1GB blocks */
-// __attribute__((section(".page_tables"), aligned(4096))) uint64_t l0_tbl[512];
-__attribute__((section(".page_tables"), aligned(4096))) uint64_t l1_tbl[512];
+/* stage 2 guest PT descripts */
+#define PTE_S2_VALID          (1UL << 0)
+#define PTE_S2_TABLE          (1UL << 1)
+#define PTE_S2_BLOCK          (0UL << 1)
+#define PTE_S2_MEMATTR_IDX(x) ((x) << 2)
+#define PTE_S2_AP_RW          (3UL << 6)
+#define PTE_S2_SH_IS          (3UL << 8)
+#define PTE_S2_AF            (1UL << 10)
 
+/* VTCR_EL2 s2 translation ctrl bits */
+#define VTCR_EL2_T0SZ(x)    ((x) & 0x3F)
+#define VTCR_EL2_PS_40_BIT   (2UL << 16)
+#define VTCR_EL2_TG0_4K      (0UL << 14)
+#define VTCR_EL2_SH0_IS      (3UL << 12)
+#define VTCR_EL2_ORGN0_WB    (1UL << 10)
+#define VTCR_EL2_IRGN0_WB     (1UL << 8)
+
+/* hypv s1 PTs and guest s2 PTs 
+   giving at least 1GB of mem */
+__attribute__((section(".page_tables"), aligned(4096))) uint64_t hyp_l1_tbl[512];
+__attribute__((section(".page_tables"), aligned(4096))) uint64_t guest_l1_pt[512];
 /* UART */
 void uart_init(void) { /* memory-mapped, no impl needed 4 qemu */ }
 void uart_putc(char c) { if (c == '\n') { UART0_DR = '\r'; } UART0_DR = c; }
 void uart_puts(const char *s) { while (*s) uart_putc(*s++); }
-void uart_put_hex(uint64_t n)
+void uart_put_hex(uint64_t n) 
 {
     char somedigits[] = "0123456789abcdef";
     uart_puts("0x");
     for (int i = 60; i >= 0; i -= 4) {
-            uart_putc(somedigits[(n >> i) & 0xf]);
+        uart_putc(somedigits[(n >> i) & 0xf]);
     }
 }
 
 /* setup MMU */
 static void mmu_init(void)
 {
-    /* L0 tbl -> L1 tbl
-           L0 entry covers the first 512GB of vaddr space
-           it's a ptr to our L1 tbl */
-    // l0_tbl[0] = PTE_VALID | PTE_TABLE | (uint64_t) l1_tbl;
-
-    /* L1 tbl -> 2GB of phys main mem
-           create two 1GB block descripts for an identtiy map
-           an L1 block descript maps a 1GB region
-
-           VA 0x00_0000_0000 -> PA 0x00_0000_0000 (1GB)
-           covers our UART addr @ 0x09000000 */
-    l1_tbl[0] = PTE_VALID | PTE_BLOCK | PTE_MEM_ATTR_IDX(0) |   /* ATTR0 -> Device */
-                   PTE_AF | PTE_SH_IS | PTE_AP_RW_EL2 | 0x00000000;
-
-    /*     VA 0x40000000     -> PA 0x40000000     (1GB)
-           covers our hypv code in main mem  */
-    l1_tbl[1] = PTE_VALID | PTE_BLOCK | PTE_MEM_ATTR_IDX(1) |   /* ATTR1 -> Normal */
-                PTE_AF | PTE_SH_IS | PTE_AP_RW_EL2 | 0x40000000;
+    /* i hope this formatting hurts your eyes */
+    hyp_l1_tbl[0] = PTE_VALID | PTE_BLOCK | PTE_MEM_ATTR_IDX(0) |   /* ATTR0 -> Device */
+                       PTE_AF | PTE_SH_IS | PTE_AP_RW_EL2 | 0x00000000;
+    hyp_l1_tbl[1] = PTE_VALID | PTE_BLOCK | PTE_MEM_ATTR_IDX(1) |   /* ATTR1 -> Normal */
+                       PTE_AF | PTE_SH_IS | PTE_AP_RW_EL2 | 0x40000000;
 
     /* configure TCR_EL2
            39-bit VA space (512GB), 4K granule, inner shareable,
            WB cacheable, 40-bit PA space */
-    uint64_t __tcr = TCR_EL2_T0SZ(25) | TCR_EL2_TG0_4K | TCR_EL2_SH0_IS | TCR_EL2_ORGN0_WB |
-                     TCR_EL2_IRGN0_WB | TCR_EL2_PS_40_BIT;
+    uint64_t __tcr = TCR_EL2_T0SZ(25) | TCR_EL2_TG0_4K    | TCR_EL2_SH0_IS   | 
+                     TCR_EL2_ORGN0_WB |  TCR_EL2_IRGN0_WB | TCR_EL2_PS_40_BIT;
     __write_tcr_el2(__tcr);
 
     /* configure MAIR_EL2
@@ -131,7 +132,7 @@ static void mmu_init(void)
     __asm__ volatile("msr mair_el2, %0" : : "r"((uint64_t) (MAIR_ATTR1_NORM << 8) | MAIR_ATTR0_DEV));
 
     /* set page table base addr */
-    __write_ttbr0_el2((uint64_t) l1_tbl);
+    __write_ttbr0_el2((uint64_t) hyp_l1_tbl);
 
     /* ensure all page table writes and config
        are actually committed */
@@ -146,11 +147,41 @@ static void mmu_init(void)
     __asm__ volatile("isb");
 }
 
+static void guest_mmu_init(void)
+{
+    uart_puts("icevmm: setting up stage 2 MMU for guest...\n");
 
+    /* ok so we give guest just a simple 1:1 identity mapping for 
+       the first 1GB
+       
+       this will require at least 2MB block entries in our L2 tbl
+       IPA 0x00000000 -> PA 0x00000000 (covers UART @ 0x09000000) 
+       IPA 0x40000000 -> PA 0x40000000 (covers our code @ 0x4000xxxx) */
 
+    /* map the first 2MB block as device mem for UART */
+    guest_l1_pt[0] = PTE_S2_VALID | PTE_S2_BLOCK | PTE_S2_MEMATTR_IDX(0) |  /* ATTR0 -> Device */
+                     PTE_S2_AP_RW | PTE_S2_SH_IS | PTE_S2_AF | 0x00000000;
+
+    /* map a another 2MB block where our hypv code is in normal mem 
+       we can calc the L2 tbl index for addr 0x40000000 
+       0x40000000 >> 21 = 512 */
+    guest_l1_pt[1] = PTE_S2_VALID | PTE_S2_BLOCK | PTE_S2_MEMATTR_IDX(1) |  /* ATTR1 -> Normal */
+                     PTE_S2_AP_RW | PTE_S2_SH_IS | PTE_S2_AF | 0x40000000;
+    
+    /* config VTCR_EL2 */
+    uint64_t __vtcr = VTCR_EL2_T0SZ(25) | VTCR_EL2_TG0_4K   | VTCR_EL2_SH0_IS   |
+                    VTCR_EL2_ORGN0_WB   | VTCR_EL2_IRGN0_WB | VTCR_EL2_PS_40_BIT;
+    __write_vtcr_el2(__vtcr);
+
+    /* set the s2 PT base addr */
+    __write_vttbr_el2((uint64_t) guest_l1_pt);
+    
+    uart_puts("icevmm: guest stage 2 MMU configured !!!\n");
+}
 
 /* test func
         for testing mmu purpooses only  */
+
 // static void mmu_test(void)
 // {
 //     uart_puts("icevmm: mmu translation test\n");
@@ -180,32 +211,24 @@ static void mmu_init(void)
 
 
 
-/* configure core EL2 regs
-        1. set EL1 to arm64
-        2. set a known-clean state for sysctrls
-        3. disable traps for SIMD/FP */
+/* configure core EL2 regs */
 static void el2_setup(void)
 {
     uart_puts("icevmm: configuring EL2\n");
-    /* 1 */
-    uint64_t __hcr = HCR_EL2_RW_BIT | (1UL << 26);  /* HCR_EL2_HVC */
+    uint64_t __hcr = HCR_EL2_RW_BIT | HCR_EL2_VM_BIT;
     __write_hcr_el2(__hcr);
-
-    /* 2 - set SCTLR_EL2 to a known-good state */
-    // __write_sctlr_el2(0x00000000);
-
-    /* 3 */
     __write_cptr_el2(0);
-
     __write_vbar_el2((uint64_t) __exception_vectors);
     uart_puts("icevmm: written vectors to vbar_el2 reg\n");
-
     uart_puts("icevmm: EL2 configured !!!\n");
 }
 
 static void vm_create(void)
 {
     uart_puts("icevmm: creating vm...\n");
+
+    /* fucking hope this works.. */
+    guest_mmu_init();
 
     /* initialise vcpu regs and setup the SPSR_EL2
        which determines the state of the cpu when we
@@ -215,8 +238,8 @@ static void vm_create(void)
        of our stack for the moment.. */
     vcpu_t *vcpu = &guest_vm.vcpu;
     vcpu->regs.elr_el2 = (uint64_t) _guest_payload;
-    vcpu->regs.spsr_el2 = 0x3c5;
-    vcpu->regs.sp_el1 = (uint64_t) __stack_top;
+    vcpu->regs.spsr_el2 = 0x3c5;    /* EL1h, all interrupts masked */
+    vcpu->regs.sp_el1 =  (uint64_t) __stack_top;
 }
 
 void handle_trap(vcpu_t *vcpu)
@@ -224,10 +247,19 @@ void handle_trap(vcpu_t *vcpu)
     uint64_t __esr;
     __asm__ volatile("mrs %0, esr_el2" : "=r"(__esr));
 
-    uart_puts("!!! TRAP HANDLER INVOKED !!!\n");
+    uart_puts("\n!!! TRAP HANDLER INVOKED !!!\n");
     uart_puts("     ESR_EL2[");
     uart_put_hex(__esr);
     uart_puts("]\n");
+
+    /* HVC handler (finally) */
+    uint32_t exception_class = __esr >> 26;
+    if (exception_class == 0x16) {
+            vcpu->regs.elr_el2 += 4;
+    } else {
+            uart_puts("icevmm:      unhandled exception. halting !!!\n");
+            hang();
+    }
 
     vcpu->regs.elr_el2 += 4;
 }
@@ -237,46 +269,23 @@ void main(void)
 {
     uart_init();
     uart_puts("\nicevmm: distant meows from baremetal aarch64 !!!\n");
-
     unsigned long __el = get_el();
     uart_puts("icevmm: current EL: ");
     uart_put_hex(__el);
     uart_puts("\n");
-
     /* check if the returned EL is not 2 
     ...it should be */
     if (__el != 2) {
             uart_puts("icevmm:      not running in EL2. halting!!!\n");
             hang();
     }
-
     uart_puts("icevmm: running in EL2\n");
-
     el2_setup();
-
     uart_puts("icevmm: enabling MMU...\n");
     mmu_init();
     uart_puts("icevmm: MMU enabled !!!\n");
-
-    // mmu_test();
-
     vm_create();
     uart_puts("icevmm: running vm...\n");
     vcpu_run(&guest_vm.vcpu);
-
-    /* after guest traps, execution resumes here */
-    uart_puts("icevmm: vm exited !!! dumping guest state...\n");
-    /* read from ESR_EL2 to inspect trap */
-    uint64_t __esr_el2;
-    __asm__ volatile("mrs %0, esr_el2" : "=r"(__esr_el2));
-    uart_puts("     ESR_EL2[");
-    uart_put_hex(__esr_el2);
-    uart_puts("]\n");
-
-    uart_puts("icevmm: halting !!!\n");
     hang();
-
-
-    // /* do nothing for now */
-    // while (1) { /* inf loop */ }
 }
